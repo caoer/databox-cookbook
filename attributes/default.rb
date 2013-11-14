67e21279516153d17c0f26117233985bc9a7f348
Jerrod Blavos <jerrod@reccenter.com>
2013-11-14 13:55:17 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,4 @@ default["databox"]["db_root_password"] = nil
 
 # A list of database_user's attribute parameters.
 # See database cookbook for details.
-default["databox"]["databases"]["mysql"] = []
 default["databox"]["databases"]["postgresql"] = []
