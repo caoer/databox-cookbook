3d803491680bcbe24ba850720dda4c0dabbce2bb
Jerrod Blavos <jerrod@reccenter.com>
2013-11-14 13:47:09 -0500

    Remove Mysql as conditional is failing

--- metadata.rb
+++ metadata.rb
@@ -7,12 +7,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
 recipe            "databox", "Includes all recipes."
-recipe            "databox::mysql", "Install MySQL and create MySQL databases."
 recipe            "databox::postgresql", "Install PostgreSQL and create PostgreSQL databases."
 
 supports "ubuntu"
 supports "debian"
 
 depends "database"
-depends "mysql"
 depends "postgresql"
